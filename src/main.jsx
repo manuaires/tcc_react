7f0558fe6a77c74197eeb557c9dfd0251fae1daf
idibs <tiagosimoesprado@gmail.com>
2025-09-19 08:04:00 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Home from "./pages/Home.jsx";
-import Cereais from "./pages/Cereais.jsx";
-import Rac from "./pages/Rações.jsx";
-import Var from "./pages/Variedades.jsx";
-import App from "./App.jsx";
-import VisualizarProdutos from "./pages/View.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Home from './pages/Home.jsx'
+import Cereais from './pages/Cereais.jsx'
+import Rac from './pages/Rações.jsx'
+import Var from './pages/Variedades.jsx'
+import App from './App.jsx'
+
 
 const router = createBrowserRouter([
   {
@@ -18,7 +18,6 @@ const router = createBrowserRouter([
       { path: "/cereais", element: <Cereais /> },
       { path: "/racoes", element: <Rac /> },
       { path: "/variedades", element: <Var /> },
-      { path: "/view/:id", element: <VisualizarProdutos /> },
     ],
   },
 ]);
